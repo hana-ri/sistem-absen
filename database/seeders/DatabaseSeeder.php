e1a3fc18977296e90de7fe94df5bbfdfa818ccdb
hana-ri <hanari@upi.edu>
2022-04-11 11:17:39 +0700

    add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'admin ku',
+            'email' => 'admin@gmail.com',
+            'password'=> bcrypt('Ggwp2001'),
+            'role' => 'Admin',
+            'status' => 1
+        ]);
     }
 }
